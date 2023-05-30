84af70de4efdc23657e6429b37a1eadf83e0dacd
Minsoo0405 <munsi0405@naver.com>
2023-05-30 19:52:26 +0900

    #17 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 테스트 메소드 수정.

--- project-board/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/domain/controller/ArticleControllerTest.java
+++ project-board/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/domain/controller/ArticleControllerTest.java
@@ -22,16 +22,16 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception{
         //Given
 
-        //When &
+        //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
 
@@ -43,7 +43,7 @@ class ArticleControllerTest {
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception{
         //Given
 
-        //When &
+        //When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
@@ -59,7 +59,7 @@ class ArticleControllerTest {
     public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception{
         //Given
 
-        //When &
+        //When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
@@ -73,7 +73,7 @@ class ArticleControllerTest {
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception{
         //Given
 
-        //When &
+        //When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
